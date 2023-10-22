b01c5b58b3124afad5a0907f1b4222e861d2d6c7
kacpersiedlarczyk <100533998+kacpersiedlarczyk@users.noreply.github.com>
2023-10-22 19:18:13 +0200

    add a css reset and some basic styling

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const gameBoard = (() => {
     const gameBoard = ["", "", "", "", "", "", "", "", ""];
 
     const render = () => {
-        const gameBoardContainer = document.querySelector("#gameboard");
+        const gameBoardContainer = document.querySelector("#board");
 
         gameBoard.forEach((square, index) => {
             gameBoardContainer.innerHTML += `
-                <div data-index="${index}">${square}</div>
+                <div class="square" data-index="${index}">${square}</div>
             `;
+        });
+
+        gameBoardContainer.addEventListener("click", event => {
+            if (event.target.className != "square") {
+                return;
+            };
+
+            console.log(event.target);
+            // game.placeMark();
         });
     };
 
@@ -23,21 +32,80 @@ function createPlayer (mark) {
     return { mark, getPoints, givePoints };
 };
 
+const score = (() => {
+    
+    const render = () => {
+        //
+    };
+
+    return { render };
+})();
+
 const game = (() => {
+    // let xTurn;
     
     const start = () => {
-
+        // const playerX = createPlayer("X");
+        // const playerO = createPlayer("O");
 
         gameBoard.render();
     };
 
-    return { start };
+    const placeMark = () => {
+        //
+    };
+
+    const reset = () => {
+        //
+    };
+
+    return { start, placeMark, reset };
 })();
 
 
 
 
 
-// document.querySelector("button").addEventListener("click", event => {
+
+// document.querySelector(".btn-new-game").addEventListener("click", event => {
 //     game.start();
 // });
+
+
+
+
+
+
+// xTurn
+// currentPlayer = playerX;
+
+// let players = [];
+
+// const playerX = createPlayer("X");
+// const playerO = createPlayer("O");
+
+// players.push(playerX);
+// players.push(playerO);
+
+// console.log(playerX.mark);
+// console.log(playerO.mark);
+
+
+// let players = [playerX, playerO];
+
+// players[0].mark => "X";
+
+// console.log(players);
+
+
+
+
+// default/first mark should be an "X"
+// start function => what needs to happen when the game starts
+// won method should take board as a argument!
+// allow player to place thier marks
+// create a function that updates the gameBoard array with corresponding mark
+// create a varible that stores all winning positions
+// style the board
+// add a hover effect with a current mark
+
